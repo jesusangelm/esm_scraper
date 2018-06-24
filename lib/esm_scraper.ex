f487a07ec8999e14fe7fce2dc1caef8848e6f9c0
Jesus Marin <jesusangelm@gmail.com>
2018-06-23 21:02:28 -0400

    some improvements

--- lib/esm_scraper.ex
+++ lib/esm_scraper.ex
@@ -5,16 +5,14 @@ defmodule EsmScraper do
 
   def parser(dom) do
     classifieds_dom = Floki.find(dom, ".classified")
-    Enum.map classifieds_dom, fn(c) -> c |> Floki.DeepText.get |> String.replace("\n", "") end
+    Enum.map classifieds_dom, &(&1 |> Floki.DeepText.get |> String.replace("\n", ""))
   end
 
-  def html_requester do
-    url = "http://www.elsoldemargarita.com.ve/clasificados/index/fsection:1"
-    HTTPoison.get! url
-  end
-
-  def get_classifieds do
-    dom = html_requester()
+  def get_classifieds(type) do
+    dom = html_requester(type)
     dom.body |> parser()
   end
+
+  def html_requester(:apartment_rent), do: "http://www.elsoldemargarita.com.ve/clasificados/index/fsection:1" |> HTTPoison.get!
+  def html_requester(:house_rent), do: "http://www.elsoldemargarita.com.ve/clasificados/index/fsection:4" |> HTTPoison.get!
 end
