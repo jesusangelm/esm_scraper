9a2fed57351c2871760a83c025df9d332ba8eefd
Jesus Marin <jesusangelm@gmail.com>
2018-06-24 17:20:49 -0400

    add test for page_ammount_inspector function

--- test/esm_scraper_test.exs
+++ test/esm_scraper_test.exs
@@ -7,4 +7,10 @@ defmodule EsmScraperTest do
     list_classifieds = EsmScraper.parser dom
     assert list_classifieds |> is_list
   end
+
+  test "page_ammount_inspector function should return a integer number" do 
+    dom = File.read!("clasificados_apartamentos.html")
+    pages = EsmScraper.page_ammount_inspector(dom)
+    assert pages |> is_integer
+  end
 end
