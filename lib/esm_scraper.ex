f29b4a6aa37f81ea3eb7afce8bdca56ef36d75bc
Jesus Marin <jesusangelm@gmail.com>
2018-07-09 10:21:49 -0400

    some improves

--- lib/esm_scraper.ex
+++ lib/esm_scraper.ex
@@ -6,21 +6,25 @@ defmodule EsmScraper do
   @url_house_rent "http://www.elsoldemargarita.com.ve/clasificados/index/fsection:4"
 
   def parser(dom) do
-    classifieds_dom = Floki.find(dom, ".classified")
-    Enum.map classifieds_dom, &(&1 |> Floki.DeepText.get |> String.replace("\n", ""))
+    dom
+    |> Floki.find(".classified")
+    |> Enum.map(&(&1 |> Floki.text |> String.replace("\n", "")))
   end
 
   def page_ammount_inspector(dom) do
-    [_,_,_, head | _] = dom |> Floki.find("hr+ p") |> Floki.text |> String.split(" ")
-    head |> String.replace(",", "") |> String.to_integer
+    [_,_,_, head | _] = dom |> Floki.find("hr+ p") |> Floki.text() |> String.split(" ")
+
+    head
+    |> String.replace(",", "")
+    |> String.to_integer()
   end
 
   def url_generator(type, pages) do
     url_base = url_selector(type)
-    list = []
-    for n <- 1..pages do
-      list = list ++ url_base <> "/page:#{n}"
-    end
+
+    Enum.reduce(pages, [], fn(page, acc) ->
+      acc ++ url_base <> "/page:#{page}"
+    end)
   end
 
   def url_selector(type) do
@@ -31,17 +35,15 @@ defmodule EsmScraper do
     end
   end
 
-  def html_requester(url) do
-    dom = url |> HTTPoison.get!
-    dom.body
-  end
+  def html_requester(url), do: HTTPoison.get!(url).body
 
   def get_classifieds(type) do
-    dom_base = url_selector(type) |> html_requester()
+    dom_base = type |> url_selector() |> html_requester()
     pages = page_ammount_inspector(dom_base)
     urls = url_generator(type, pages)
 
-    classifieds = Enum.map urls, &(&1 |> html_requester |> parser())
-    classifieds |> List.flatten
+    urls
+    |> Enum.map(&(&1 |> html_requester() |> parser()))
+    |> List.flatten()
   end
 end
